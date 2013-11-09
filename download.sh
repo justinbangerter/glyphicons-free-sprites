665dd89c4769f343d4bbe64cda0d8762a3caecb7
justinbangerter <jnbangerter@gmail.com>
2013-11-09 10:23:57 -0500

    added download script

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' http://glyphicons.com/files/glyphicons_free.zip
+unzip glyphicons_free.zip
+rm glyphicons_free.zip
